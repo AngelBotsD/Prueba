c9825502faddc529b4c29b7450c92a9eca2fe762
AngelBotsD <angelperez22448@gmail.com>
2025-12-09 20:49:13 -0600

    Actualizar grupos-config.js

--- plugins/grupos-config.js
+++ plugins/grupos-config.js
@@ -1,46 +1,15 @@
-let handler = async (m, { conn, isAdmin, isBotAdmin }) => {
-  const sticker = m.message?.stickerMessage;
+let handler = async (m, { conn }) => {
+  const quoted = m.message?.extendedTextMessage?.contextInfo?.quotedMessage;
+  const st = quoted?.stickerMessage;
 
-  if (!sticker) return;
+  if (!st) return conn.sendMessage(m.chat, { text: "Responde a un sticker." });
 
-  // ID del sticker (array → buffer)
-  const cerrarID = Buffer.from([
-    3,74,169,113,129,224,130,216,68,22,163,31,155,2,
-    77,54,200,19,222,61,146,168,204,106,77,248,131,
-    213,117,146,94,54
-  ]);
+  const base64 = st.fileSha256.toString("base64");
 
-  // Obtener SHA del sticker recibido
-  const sha = sticker.fileSha256;
-
-  // Comparar
-  if (!sha || !sha.equals(cerrarID)) return;
-
-  // Validar permisos
-  if (!isAdmin)
-    return conn.sendMessage(m.chat, { text: "Solo admins pueden usar este sticker." }, { quoted: m });
-
-  if (!isBotAdmin)
-    return conn.sendMessage(m.chat, { text: "Dame admin para cerrar el grupo." }, { quoted: m });
-
-  // Cerrar grupo
-  await conn.groupSettingUpdate(m.chat, "announcement");
-
-  // Sticker de confirmación
-  await conn.sendMessage(m.chat, {
-    sticker: { url: "https://cdn.russellxz.click/1f922165.webp" },
-    quoted: m
-  });
-
-  // Reacción
-  await conn.sendMessage(m.chat, {
-    react: { text: "✅", key: m.key }
-  });
+  return conn.sendMessage(m.chat, {
+    text: `🆔 ID BASE64:\n\`\`\`${base64}\`\`\``
+  }, { quoted: m });
 };
 
-
-handler.command = new RegExp(); // obligatorio
-handler.customPrefix = /^$/; // no activa por texto
-handler.group = true;
-
+handler.command = ["id64"];
 export default handler;
