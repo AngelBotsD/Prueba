c29a9ce870c9e3a001a3faa2b5701192b85f45de
AngelBotsD <angelperez22448@gmail.com>
2025-12-09 20:50:30 -0600

    Actualizar grupos-config.js

--- plugins/grupos-config.js
+++ plugins/grupos-config.js
@@ -1,15 +1,38 @@
 let handler = async (m, { conn }) => {
-  const quoted = m.message?.extendedTextMessage?.contextInfo?.quotedMessage;
-  const st = quoted?.stickerMessage;
+  const text = m.text?.trim().toLowerCase() || ""
+  let action = text.match(/(abrir|cerrar|open|close)/)
+  if (!action) return
 
-  if (!st) return conn.sendMessage(m.chat, { text: "Responde a un sticker." });
+  action = action[1]
 
-  const base64 = st.fileSha256.toString("base64");
+  let mode = /abrir|open/.test(action)
+    ? "not_announcement"
+    : "announcement"
 
-  return conn.sendMessage(m.chat, {
-    text: `🆔 ID BASE64:\n\`\`\`${base64}\`\`\``
-  }, { quoted: m });
-};
+  await conn.groupSettingUpdate(m.chat, mode)
 
-handler.command = ["id64"];
-export default handler;
+  // === Sticker desde bytes ===
+  const stickerBytes = [
+    3,74,169,113,129,224,130,216,68,22,163,31,155,2,77,54,
+    200,19,222,61,146,168,204,106,77,248,131,213,117,146,94,54
+  ]
+
+  const bufferSticker = Buffer.from(stickerBytes)
+
+  await conn.sendMessage(m.chat, {
+    sticker: bufferSticker,
+    quoted: m
+  })
+
+  await conn.sendMessage(m.chat, {
+    react: { text: '✅', key: m.key }
+  })
+}
+
+handler.help = ["𝖦𝗋𝗎𝗉𝗈 𝖠𝖻𝗋𝗂𝗋", "𝖦𝗋𝗎𝗉𝗈 𝖢𝖾𝗋𝗋𝖺𝗋"]
+handler.tags = ["𝖦𝖱𝖴𝖯𝖮𝖲"]
+handler.customPrefix = /^(?:\.?grupo\s*(abrir|cerrar|open|close)|\.?(abrir|cerrar|open|close))$/i
+handler.command = new RegExp()
+handler.group = true
+
+export default handler
